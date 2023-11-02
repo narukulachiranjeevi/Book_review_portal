c0136bc12e3b1f6be2c86c3e87b687a550a9f228
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-11-02 18:28:54 +0530

    cleaned the git

--- book_portal/books/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Book,Comment,Reply
-# Register your models here.
-admin.site.register(Book)
-admin.site.register(Comment)
-admin.site.register(Reply)
